c4398985ca7c38dd66076f3ffdc8252471f09b20
Sarthak2905 <sarthakhendre61@gmail.com>
2026-07-04 00:25:09 +0530

    update axios

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -4,7 +4,6 @@ console.log("API URL:", import.meta.env.VITE_API_URL);
 
 const api = axios.create({
    baseURL: import.meta.env.VITE_API_URL,
-    withCredentials: true,
 })
 
 api.interceptors.request.use((config) => {
