24d30cc795b7fb8f62ca53ea0acdad7ec6fd638c
Sarthak2905 <sarthakhendre61@gmail.com>
2026-07-03 23:18:08 +0530

    backend deployment link add

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://business-management-backend-6ug7.onrender.com',
         changeOrigin: true,
       },
     },
